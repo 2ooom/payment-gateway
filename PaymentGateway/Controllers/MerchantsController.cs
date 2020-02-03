89ee553b2cfa4a1aa9133303482ae958a5fcc201
2ooom <dmitry.parfenchik@gmail.com>
2020-02-03 09:09:46 +0100

    Facoring Authentication into MerchantsController

--- PaymentGateway/Controllers/MerchantsController.cs
+++ PaymentGateway/Controllers/MerchantsController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using PaymentGateway.Model;
-using PaymentGateway.Services;
 
 namespace PaymentGateway.Controllers
 {
@@ -14,29 +16,91 @@ namespace PaymentGateway.Controllers
     [ApiController]
     public class MerchantsController : ControllerBase
     {
-        private readonly IAuthenticationService _authService;
+        private readonly IPaymentDbContext _paymentDb;
+        private readonly IConfig _config;
 
-        public MerchantsController(IAuthenticationService authService)
+        private const int NbHashIterations = 1000;
+        internal static readonly TimeSpan TokenValidity = TimeSpan.FromDays(7);
+
+        public MerchantsController(IPaymentDbContext paymentDb, IConfig config)
         {
-            _authService = authService;
+            _paymentDb = paymentDb;
+            _config = config;
         }
 
         [HttpPost]
-        public async Task<ActionResult<MerchantCreationRequest>> CreateMerchant([FromBody]MerchantCreationRequest request)
+        public async Task<ActionResult<MerchantCreationResponse>> CreateMerchant([FromBody]MerchantCreationRequest request)
         {
-            var response = await _authService.CreateMerchant(request);
-            return Ok(response);
+            var existingMerchant = await _paymentDb.Merchants.FirstOrDefaultAsync(m => m.Login == request.Login);
+            if (existingMerchant != null)
+            {
+                return BadRequest(new { message = "Merchant with this login already exists" });
+            }
+            var salt = GenerateSalt();
+            var merchant = new Merchant
+            {
+                Name = request.Name,
+                Login = request.Login,
+                Url = request.Url,
+                AcquirerType = request.AcquirerType,
+                Salt = salt,
+                HashedPassword = GetHashedPassword(request.Password, salt),
+                Active = true,
+            };
+            _paymentDb.Merchants.Add(merchant);
+
+            await _paymentDb.SaveChangesAsync();
+
+            return new MerchantCreationResponse { Id = merchant.Id };
+        }
+
+        private static string GenerateSalt()
+        {
+            var salt = new byte[128 / 8];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+
+            return Convert.ToBase64String(salt);
         }
 
         [HttpPost("authenticate")]
         public async Task<ActionResult<AuthenticationResponse>> Authenticate([FromBody]AuthenticationRequest request)
         {
-            var response = await _authService.Authenticate(request);
-
-            if (response == null)
+            var merchant = await _paymentDb.Merchants.SingleOrDefaultAsync(x => x.Login == request.Login);
+            if (merchant == null || GetHashedPassword(request.Password, merchant.Salt) != merchant.HashedPassword)
+            {
                 return BadRequest(new { message = "Username or password is incorrect" });
+            }
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_config.JwtSecret);
+            var expires = DateTime.UtcNow.Add(TokenValidity);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, merchant.Id.ToString())
+                }),
+                Expires = expires,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return new AuthenticationResponse
+            {
+                Expires = expires,
+                JwtToken = tokenHandler.WriteToken(token),
+            };
+        }
 
-            return Ok(response);
+        internal static string GetHashedPassword(string password, string salt)
+        {
+            return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: Convert.FromBase64String(salt),
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: NbHashIterations,
+                numBytesRequested: 256 / 8));
         }
     }
 }
--- PaymentGateway/Startup.cs
+++ PaymentGateway/Startup.cs
@@ -1,27 +1,19 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using PaymentGateway.Acquiring;
 using PaymentGateway.Model;
-using PaymentGateway.Services;
 
 namespace PaymentGateway
 {
     public class Startup
     {
-        private string _connectionString = "Data Source=payment.db";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -39,7 +31,6 @@ namespace PaymentGateway
                 => opt.UseSqlite(config.PaymentDbConnectionString));
             // Dependency Registration
             services.AddSingleton<IConfig>(config);
-            services.AddScoped<IAuthenticationService, AuthenticationService>();
             services.AddScoped<IBankRegistry, BankRegistry>();
 
 
